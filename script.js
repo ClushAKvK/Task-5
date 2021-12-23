87e4fcd9f7a93f560f9f767d6fccd2c53373a1fc
ClushAKvK <90634189+ClushAKvK@users.noreply.github.com>
2021-12-23 19:17:04 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function cal()
-{
-   price = document.getElementById('product_price').value;
-   count = document.getElementById('product_count').value;
-   mult = parseInt(price) * parseInt(count);
-   document.getElementById('mult').innerHTML="Стоимость: "+ mult + " руб.";
-   return false;
-}
